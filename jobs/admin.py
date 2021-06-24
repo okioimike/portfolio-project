df6342f171da78c6ad60a9acfc4def9d353d63d0
okioimike <okioibonyi@gmail.com>
2021-06-24 03:08:01 +0300

    added static files containing image also added a bootstarp theme for design

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
-admin.site.register(Job)
+#admin.site.register(Job)
+@admin.register(Job)
+class jobAdmin(admin.ModelAdmin):
+    list_display = ['image', 'summary']
