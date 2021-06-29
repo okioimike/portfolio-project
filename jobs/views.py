8f4203cc6849529d020929568a5bf86fa023ddc1
okioimike <okioibonyi@gmail.com>
2021-06-30 02:06:49 +0300

    Added links in job details

--- jobs/views.py
+++ jobs/views.py
@@ -9,4 +9,4 @@ def homepage(request):
 
 def detail(request,job_id):
     job_detail = get_object_or_404(Job, pk=job_id)
-    return render(request,'jobs/home.html')
+    return render(request,'jobs/detail.html', {'job':job_detail})
